f0454ebe59a79a03fbf6c3a7cd21f76056ba61c0
guramijibladze <guramijibladze@gmail.com>
2021-03-17 11:34:24 +0400

    up

---
+++ online-shop/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddProductComponent } from './add-product/add-product.component';
+import { ProductComponent } from './product/product.component';
+
+const routes: Routes = [
+  { path: '', component: AddProductComponent },
+  { path: 'product', component: ProductComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export const routingComponents = [ AddProductComponent, ProductComponent ]
